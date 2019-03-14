eadcc4a73c93c85395c8f7d520a9649c44938438
Your Name <you@example.com>
2019-03-14 10:24:31 +0330

    AUTH Time Delta Increased

--- AILab/settings.py
+++ AILab/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '@j&^@=4t9r&^!@9dq_!_pga=y63#(5oj9tqdk3kvg3r%4ux$d#'
 
-AUTH_TIME_DELTA = 15*60
+AUTH_TIME_DELTA = 30*60
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
